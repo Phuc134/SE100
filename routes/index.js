7aed0a40f47025faa72b4cd5bc91fd0fbbe5d184
Tú Võ Xuân <19522461@ms.uit.edu.vn>
2021-11-12 15:31:54 +0700

    CRUD-Supplier

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const customerRouter = require('./customer');
+const supplierRouter = require('./supplier')
 const siteRouter = require('./site');
 
 function route (app) {
     
     app.use('/customers', customerRouter);
+    app.use('/suppliers', supplierRouter);
     app.use('/', siteRouter);
     
 }
---
+++ routes/supplier.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const supplierController = require('../controllers/supplier');
+
+router.get('/', supplierController.index);
+router.get('/create', supplierController.create);
+router.post('/store', supplierController.store);
+router.get('/:id/edit', supplierController.edit);
+router.put('/:id', supplierController.update);
+router.delete('/:id', supplierController.delete);
+
+module.exports = router;
