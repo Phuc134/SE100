4ca374a1f4d8cc23adf986ca7a59af6428d87fd4
vominhtuan1 <19522487@gm.uit.edu.vn>
2021-11-16 09:13:08 +0700

    validate customer

--- routes/customer.js
+++ routes/customer.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const validate = require('../validator/validator');
 const customerController = require('../controllers/customer');
 
 router.get('/create', customerController.create);
-router.post('/store', customerController.store);
+router.post('/store', validate.validate.validateCreateCustomer(),customerController.store);
 router.get('/:id/edit', customerController.edit);
-router.put('/:id', customerController.update);
+router.put('/:id', validate.validate.validateEditCustomer(), customerController.update);
 router.delete('/:id', customerController.delete);
 router.get('/:slug', customerController.index);
 router.get('/', customerController.index);
--- validator/validator.js
+++ validator/validator.js
@@ -1,6 +1,7 @@
 const {check} = require('express-validator');
 const {body} = require('express-validator');
 const supplier = require('../models/supplier');
+const customer = require('../models/customer');
 
 
 let validateAddSupplier = () => {
@@ -47,9 +48,62 @@ let validateUpdateSupplier = () => {
     ]
 }
 
+let validateCreateCustomer = () => {
+    return[
+        check('name', "Tên không được bỏ trống.").notEmpty(),
+        check('phone', "Số điện thoại không được bỏ trống.").notEmpty(),
+        check('phone', "Số điện thoại phải là số.").isNumeric(),
+        check('phone', "Số điện thoại chỉ bao gồm 10 số.").isLength({min : 10, max : 10}),
+        check('email', "Email không được bỏ trống.").notEmpty(),
+        check('email', "Email không đúng định dạng.").isEmail(),
+        check('address', "Địa chỉ không được bỏ trống.").notEmpty(),
+        body('phone').custom( async value => {
+            const p = await customer.findOne({ phone: value });
+            if (p) {
+                return Promise.reject('Số điện thoại này đã được sử dụng.');
+            }
+        }),
+        body('email').custom( async value => {
+            const p = await customer.findOne({ email: value });
+            if (p) {
+                return Promise.reject('Email này đã được sử dụng.');
+            }
+        }),
+    ];
+}
+let validateEditCustomer = () => {
+    return [
+        check('name', "Tên không được bỏ trống").notEmpty(),
+        check('phone', "Số điện thoại không được bỏ trống").notEmpty(),
+        check('phone', "Số điện thoại phải là số.").isNumeric(),
+        check('phone', "Số điện thoại chỉ bao gồm 10 số.").isLength({min : 10, max : 10}),
+        check('email', "Email không được bỏ trống").notEmpty(),
+        check('email', "Email không đúng định dạng").isEmail(),
+        check('address', "Địa chỉ không được bỏ trống").notEmpty(),
+        body('phone').custom( async (value, {req}) => {
+            const p = await customer.findById({_id : req.params.id});
+            const q = await customer.findOne({phone : value});
+
+            if(q && q.phone!= p.phone) {
+                return Promise.reject('Số điện thoại này đã được sử dụng.');
+            }
+        }),
+        body('email').custom( async (value, {req}) => {
+            const p = await customer.findById({_id : req.params.id});
+            const q = await customer.findOne({email : value});
+
+            if(q && q.email != p.email) {
+                return Promise.reject('Email này đã được sử dụng.');
+            }
+        })
+    ];
+}
+
 let validate = {
     validateAddSupplier : validateAddSupplier,
-    validateUpdateSupplier : validateUpdateSupplier
+    validateUpdateSupplier : validateUpdateSupplier,
+    validateCreateCustomer : validateCreateCustomer,
+    validateEditCustomer : validateEditCustomer
 };
 
 module.exports = {validate};
