8dffac02ee2a43aafff3836b9cb1b1ee6c8ef72d
vominhtuan1 <19522487@gm.uit.edu.vn>
2021-11-18 16:42:55 +0700

    ajax cho customer

--- models/customer.js
+++ models/customer.js
@@ -5,7 +5,8 @@ const customer = new Schema({
     name: { type : String, required: true},
     phone: {type : String, required: true},
     address: {type: String, required: true},
-    email: {type : String, required: true}
+    email: {type : String, required: true},
+    lastContact : {type : String, default : "Chưa mua hàng"}
 });
 
 module.exports = mongoose.model('customer',customer); 
