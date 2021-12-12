0171487263ac54b58bbb321c5fa416f80394c1e0
Lý Quang Minh <69589876+Q-mily@users.noreply.github.com>
2021-12-13 04:07:35 +0700

    update banhang

--- controllers/order.js
+++ controllers/order.js
@@ -2,11 +2,112 @@ const {multipleMongooseToObject} = require('../util/mongoose');
 const {mongooseToObject} = require('../util/mongoose');
 //const User = require('../models/user');
 //const moment = require('moment');
+const Product = require('../models/product');
+const TypeProduct = require('../models/typeproduct');
+
+const Order = require('../models/pos/order');
+const Order_detail = require('../models/pos/order_detail');
+const Invoice = require('../models/pos/invoice');
+
 class OrderController {
     // [GET] /
-    index(req, res,next) {
-        res.render('order/orderlist');
+    async index(req, res,next) {
+        var orders_isnotpay = await Order.find({isPay:false}).lean();
+        var invoices = await Invoice.find({isPay: true}).lean();
+
+        res.render('order/orderlist',{
+            orders_isnotpay : orders_isnotpay,
+            orders_ispay : invoices
+        });
+    }
+
+    // [GET] / pos
+    async pos(req,res, next){
+        var data=[];
+        var sub;
+        var products;
+        var types = await TypeProduct.find({}).lean();
         
+        var promise = new Promise(
+            async function(resolve, reject){
+                for (let i = 0; i< types.length; i++){
+                    sub = {
+                        category: types[i].displayname,
+                        items:[],
+                    };
+                    sub.items = await Product.find({idType: types[i].idType}).lean();
+                    data.push(sub);
+                }
+                resolve(data);
+            }
+        );
+
+        promise.then(function(data){         
+            res.render('order/pos', {data: data});
+        })
+    }
+
+    // [POST] /oders/orderandpay
+    async OrderAndPay(req,res,next){
+        var data;
+        var items = req.body.order_details;
+
+        var order = await Order.create({isPay: true});
+        var invoice_data = {
+            idOrder: order.idOrder,
+            total : req.body.totalCart,
+            quantity: req.body.totalQuantity,
+            discount: req.body.totalDiscount,
+            total_payment: req.body.totalPayment,
+        }
+        await items.forEach( item =>{
+            data = {
+                idOrder: order.idOrder,
+                idProduct: item.id,
+                quantity: item.quantity,
+                unitPrice: item.unitprice,
+                unitDiscount: item.discount,
+                total: item.total,
+                total_payment: item.totalEND,
+            };
+            Order_detail.create(data);
+        });
+        await Invoice.create(invoice_data);
+
+        res.status(201).json({
+            msg: 'Đã thanh toán'
+        });
+    }
+
+    async deleteOrder(req, res, next){
+        var ID = req.params.id;
+        await Order_detail.deleteMany({idOrder: ID});
+        await Order.deleteMany({idOrder: ID});
+        res.redirect('/orders');
+    }
+
+     // [POST] /oders/order     //chua xong
+    async Order(req, res, next){
+        var data;
+        var items = req.body.order_details;
+
+        var order = await Order.create({isPay: false});
+        await items.forEach( item =>{
+            data = {
+                idOrder: order.idOrder,
+                idProduct: item.id,
+                quantity: item.quantity,
+                unitPrice: item.unitprice,
+                unitDiscount: item.discount,
+                total: item.total,
+                total_payment: item.totalEND,
+            };
+            Order_detail.create(data);
+        });
+
+        res.status(201).json({
+            msg: 'Đã Lưu lại!'
+        });
     }
 }
 
--- routes/order.js
+++ routes/order.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/order');
 
+router.get('/pos', orderController.pos);
 router.get('/', orderController.index);
 
-
+router.post('/:id/delete', orderController.deleteOrder);
+router.post('/orderandpay', orderController.OrderAndPay);
+router.post('/order', orderController.Order);
 module.exports = router;
