d786bc1ca26d879750f1c2e0e1dd68343e048f49
19520225 <19520225@gm.uit.edu.vn>
2021-12-19 23:29:57 +0700

    .

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.engine('hbs', handlebars({
 }));
 app.set('view engine','hbs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
     express.urlencoded({
         extended: true,
